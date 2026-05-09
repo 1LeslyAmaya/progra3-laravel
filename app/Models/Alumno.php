805c5db33e3ca6173be0bad8008f8047a7b758d3
JP <ingejp@outlook.com>
2026-05-09 07:38:47 -0600

    se desarrolla el modelo alumno con sus atributos y metodo casts para la fecha y se agrega metodo para devolver nombre completo

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alumno extends Model
 {
-    //
+    protected $fillable = [
+        'carnet',
+        'nombres',
+        'apellidos',
+        'email',
+        'telefono',
+        'fecha_nacimiento',
+        'carrera',
+        'semestre',
+        'estado',
+    ];
+
+    protected $casts = [
+        'fecha_nacimiento' => 'date',
+    ];
+
+    public function getNombreCompletoAttribute(): string
+    {
+        return "{$this->nombres} {$this->apellidos}";
+    }
 }
